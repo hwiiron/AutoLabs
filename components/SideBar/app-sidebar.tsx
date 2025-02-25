dd24995ddf9c2f1b148431e61922ae7fe3b88cf3
HwichulShin <shinhwiiron@gmail.com>
2025-02-25 22:26:47 +0900

    Sidebar 관련 컴포넌트 폴더 추가

---
+++ components/SideBar/app-sidebar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import * as React from "react";
+import data from "@/mocks/sidebarData";
+import Link from "next/link";
+import Logo from "@/public/images/icon-logo-gradient.svg";
+import { NavMain } from "@/components/SideBar/nav-main";
+import { NavUser } from "@/components/SideBar/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="none" {...props} className="h-screen">
+      <SidebarHeader>
+        <Link href={"/"} className="relative w-[120px]">
+          <Logo />
+        </Link>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+
+      <SidebarRail />
+    </Sidebar>
+  );
+}
