830fed96404f53b2b99f5aaa751e19c42a69e079
HwichulShin <shinhwiiron@gmail.com>
2025-02-26 12:06:03 +0900

    모바일 해상도 로고 사이즈 조정

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -6,7 +6,7 @@ function ComingSoon() {
   return (
     <div className="fixed left-0 top-0 z-[3] flex h-screen w-full items-center justify-center bg-black">
       <div className="flex flex-col items-center">
-        <div className="w-[180px] mo:w-[80px]">
+        <div className="w-[180px] mo:w-[150px]">
           <Logo type="icon" />
         </div>
         <p className="mt-[16px] text-2xl font-medium text-white">
