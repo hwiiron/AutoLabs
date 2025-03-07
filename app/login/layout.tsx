eca295bbc945536545aa3516887680f0d99df0cb
HwichulShin <shinhwiiron@gmail.com>
2025-03-07 14:24:50 +0900

    로그인 경로 변경

---
+++ app/login/layout.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import "@/styles/globals.css";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export const metadata: Metadata = {
+  title: "AutoLabs - Login",
+  description: "AI 기술을 활용한 섬유 결함 검수 플랫폼",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>
+        <SidebarProvider>
+          <SidebarInset>{children}</SidebarInset>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
