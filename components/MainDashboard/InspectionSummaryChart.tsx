f9e4d5a1b51b9c44c2b94a30f2152e365fa42fba
HwichulShin <shinhwiiron@gmail.com>
2025-03-07 21:50:27 +0900

    검수 통계 데이터 key 변경

--- components/MainDashboard/InspectionSummaryChart.tsx
+++ components/MainDashboard/InspectionSummaryChart.tsx
@@ -27,108 +27,108 @@ import {
 } from "@/components/ui/select";
 
 const chartData = [
-  { date: "2024-04-01", desktop: 222, mobile: 150 },
-  { date: "2024-04-02", desktop: 97, mobile: 180 },
-  { date: "2024-04-03", desktop: 167, mobile: 120 },
-  { date: "2024-04-04", desktop: 242, mobile: 260 },
-  { date: "2024-04-05", desktop: 373, mobile: 290 },
-  { date: "2024-04-06", desktop: 301, mobile: 340 },
-  { date: "2024-04-07", desktop: 245, mobile: 180 },
-  { date: "2024-04-08", desktop: 409, mobile: 320 },
-  { date: "2024-04-09", desktop: 59, mobile: 110 },
-  { date: "2024-04-10", desktop: 261, mobile: 190 },
-  { date: "2024-04-11", desktop: 327, mobile: 350 },
-  { date: "2024-04-12", desktop: 292, mobile: 210 },
-  { date: "2024-04-13", desktop: 342, mobile: 380 },
-  { date: "2024-04-14", desktop: 137, mobile: 220 },
-  { date: "2024-04-15", desktop: 120, mobile: 170 },
-  { date: "2024-04-16", desktop: 138, mobile: 190 },
-  { date: "2024-04-17", desktop: 446, mobile: 360 },
-  { date: "2024-04-18", desktop: 364, mobile: 410 },
-  { date: "2024-04-19", desktop: 243, mobile: 180 },
-  { date: "2024-04-20", desktop: 89, mobile: 150 },
-  { date: "2024-04-21", desktop: 137, mobile: 200 },
-  { date: "2024-04-22", desktop: 224, mobile: 170 },
-  { date: "2024-04-23", desktop: 138, mobile: 230 },
-  { date: "2024-04-24", desktop: 387, mobile: 290 },
-  { date: "2024-04-25", desktop: 215, mobile: 250 },
-  { date: "2024-04-26", desktop: 75, mobile: 130 },
-  { date: "2024-04-27", desktop: 383, mobile: 420 },
-  { date: "2024-04-28", desktop: 122, mobile: 180 },
-  { date: "2024-04-29", desktop: 315, mobile: 240 },
-  { date: "2024-04-30", desktop: 454, mobile: 380 },
-  { date: "2024-05-01", desktop: 165, mobile: 220 },
-  { date: "2024-05-02", desktop: 293, mobile: 310 },
-  { date: "2024-05-03", desktop: 247, mobile: 190 },
-  { date: "2024-05-04", desktop: 385, mobile: 420 },
-  { date: "2024-05-05", desktop: 481, mobile: 390 },
-  { date: "2024-05-06", desktop: 498, mobile: 520 },
-  { date: "2024-05-07", desktop: 388, mobile: 300 },
-  { date: "2024-05-08", desktop: 149, mobile: 210 },
-  { date: "2024-05-09", desktop: 227, mobile: 180 },
-  { date: "2024-05-10", desktop: 293, mobile: 330 },
-  { date: "2024-05-11", desktop: 335, mobile: 270 },
-  { date: "2024-05-12", desktop: 197, mobile: 240 },
-  { date: "2024-05-13", desktop: 197, mobile: 160 },
-  { date: "2024-05-14", desktop: 448, mobile: 490 },
-  { date: "2024-05-15", desktop: 473, mobile: 380 },
-  { date: "2024-05-16", desktop: 338, mobile: 400 },
-  { date: "2024-05-17", desktop: 499, mobile: 420 },
-  { date: "2024-05-18", desktop: 315, mobile: 350 },
-  { date: "2024-05-19", desktop: 235, mobile: 180 },
-  { date: "2024-05-20", desktop: 177, mobile: 230 },
-  { date: "2024-05-21", desktop: 82, mobile: 140 },
-  { date: "2024-05-22", desktop: 81, mobile: 120 },
-  { date: "2024-05-23", desktop: 252, mobile: 290 },
-  { date: "2024-05-24", desktop: 294, mobile: 220 },
-  { date: "2024-05-25", desktop: 201, mobile: 250 },
-  { date: "2024-05-26", desktop: 213, mobile: 170 },
-  { date: "2024-05-27", desktop: 420, mobile: 460 },
-  { date: "2024-05-28", desktop: 233, mobile: 190 },
-  { date: "2024-05-29", desktop: 78, mobile: 130 },
-  { date: "2024-05-30", desktop: 340, mobile: 280 },
-  { date: "2024-05-31", desktop: 178, mobile: 230 },
-  { date: "2024-06-01", desktop: 178, mobile: 200 },
-  { date: "2024-06-02", desktop: 470, mobile: 410 },
-  { date: "2024-06-03", desktop: 103, mobile: 160 },
-  { date: "2024-06-04", desktop: 439, mobile: 380 },
-  { date: "2024-06-05", desktop: 88, mobile: 140 },
-  { date: "2024-06-06", desktop: 294, mobile: 250 },
-  { date: "2024-06-07", desktop: 323, mobile: 370 },
-  { date: "2024-06-08", desktop: 385, mobile: 320 },
-  { date: "2024-06-09", desktop: 438, mobile: 480 },
-  { date: "2024-06-10", desktop: 155, mobile: 200 },
-  { date: "2024-06-11", desktop: 92, mobile: 150 },
-  { date: "2024-06-12", desktop: 492, mobile: 420 },
-  { date: "2024-06-13", desktop: 81, mobile: 130 },
-  { date: "2024-06-14", desktop: 426, mobile: 380 },
-  { date: "2024-06-15", desktop: 307, mobile: 350 },
-  { date: "2024-06-16", desktop: 371, mobile: 310 },
-  { date: "2024-06-17", desktop: 475, mobile: 520 },
-  { date: "2024-06-18", desktop: 107, mobile: 170 },
-  { date: "2024-06-19", desktop: 341, mobile: 290 },
-  { date: "2024-06-20", desktop: 408, mobile: 450 },
-  { date: "2024-06-21", desktop: 169, mobile: 210 },
-  { date: "2024-06-22", desktop: 317, mobile: 270 },
-  { date: "2024-06-23", desktop: 480, mobile: 530 },
-  { date: "2024-06-24", desktop: 132, mobile: 180 },
-  { date: "2024-06-25", desktop: 141, mobile: 190 },
-  { date: "2024-06-26", desktop: 434, mobile: 380 },
-  { date: "2024-06-27", desktop: 448, mobile: 490 },
-  { date: "2024-06-28", desktop: 149, mobile: 200 },
-  { date: "2024-06-29", desktop: 103, mobile: 160 },
-  { date: "2024-06-30", desktop: 446, mobile: 400 },
+  { date: "2024-04-01", totalInspections: 222, detectedDefects: 150 },
+  { date: "2024-04-02", totalInspections: 97, detectedDefects: 180 },
+  { date: "2024-04-03", totalInspections: 167, detectedDefects: 120 },
+  { date: "2024-04-04", totalInspections: 242, detectedDefects: 260 },
+  { date: "2024-04-05", totalInspections: 373, detectedDefects: 290 },
+  { date: "2024-04-06", totalInspections: 301, detectedDefects: 340 },
+  { date: "2024-04-07", totalInspections: 245, detectedDefects: 180 },
+  { date: "2024-04-08", totalInspections: 409, detectedDefects: 320 },
+  { date: "2024-04-09", totalInspections: 59, detectedDefects: 110 },
+  { date: "2024-04-10", totalInspections: 261, detectedDefects: 190 },
+  { date: "2024-04-11", totalInspections: 327, detectedDefects: 350 },
+  { date: "2024-04-12", totalInspections: 292, detectedDefects: 210 },
+  { date: "2024-04-13", totalInspections: 342, detectedDefects: 380 },
+  { date: "2024-04-14", totalInspections: 137, detectedDefects: 220 },
+  { date: "2024-04-15", totalInspections: 120, detectedDefects: 170 },
+  { date: "2024-04-16", totalInspections: 138, detectedDefects: 190 },
+  { date: "2024-04-17", totalInspections: 446, detectedDefects: 360 },
+  { date: "2024-04-18", totalInspections: 364, detectedDefects: 410 },
+  { date: "2024-04-19", totalInspections: 243, detectedDefects: 180 },
+  { date: "2024-04-20", totalInspections: 89, detectedDefects: 150 },
+  { date: "2024-04-21", totalInspections: 137, detectedDefects: 200 },
+  { date: "2024-04-22", totalInspections: 224, detectedDefects: 170 },
+  { date: "2024-04-23", totalInspections: 138, detectedDefects: 230 },
+  { date: "2024-04-24", totalInspections: 387, detectedDefects: 290 },
+  { date: "2024-04-25", totalInspections: 215, detectedDefects: 250 },
+  { date: "2024-04-26", totalInspections: 75, detectedDefects: 130 },
+  { date: "2024-04-27", totalInspections: 383, detectedDefects: 420 },
+  { date: "2024-04-28", totalInspections: 122, detectedDefects: 180 },
+  { date: "2024-04-29", totalInspections: 315, detectedDefects: 240 },
+  { date: "2024-04-30", totalInspections: 454, detectedDefects: 380 },
+  { date: "2024-05-01", totalInspections: 165, detectedDefects: 220 },
+  { date: "2024-05-02", totalInspections: 293, detectedDefects: 310 },
+  { date: "2024-05-03", totalInspections: 247, detectedDefects: 190 },
+  { date: "2024-05-04", totalInspections: 385, detectedDefects: 420 },
+  { date: "2024-05-05", totalInspections: 481, detectedDefects: 390 },
+  { date: "2024-05-06", totalInspections: 498, detectedDefects: 520 },
+  { date: "2024-05-07", totalInspections: 388, detectedDefects: 300 },
+  { date: "2024-05-08", totalInspections: 149, detectedDefects: 210 },
+  { date: "2024-05-09", totalInspections: 227, detectedDefects: 180 },
+  { date: "2024-05-10", totalInspections: 293, detectedDefects: 330 },
+  { date: "2024-05-11", totalInspections: 335, detectedDefects: 270 },
+  { date: "2024-05-12", totalInspections: 197, detectedDefects: 240 },
+  { date: "2024-05-13", totalInspections: 197, detectedDefects: 160 },
+  { date: "2024-05-14", totalInspections: 448, detectedDefects: 490 },
+  { date: "2024-05-15", totalInspections: 473, detectedDefects: 380 },
+  { date: "2024-05-16", totalInspections: 338, detectedDefects: 400 },
+  { date: "2024-05-17", totalInspections: 499, detectedDefects: 420 },
+  { date: "2024-05-18", totalInspections: 315, detectedDefects: 350 },
+  { date: "2024-05-19", totalInspections: 235, detectedDefects: 180 },
+  { date: "2024-05-20", totalInspections: 177, detectedDefects: 230 },
+  { date: "2024-05-21", totalInspections: 82, detectedDefects: 140 },
+  { date: "2024-05-22", totalInspections: 81, detectedDefects: 120 },
+  { date: "2024-05-23", totalInspections: 252, detectedDefects: 290 },
+  { date: "2024-05-24", totalInspections: 294, detectedDefects: 220 },
+  { date: "2024-05-25", totalInspections: 201, detectedDefects: 250 },
+  { date: "2024-05-26", totalInspections: 213, detectedDefects: 170 },
+  { date: "2024-05-27", totalInspections: 420, detectedDefects: 460 },
+  { date: "2024-05-28", totalInspections: 233, detectedDefects: 190 },
+  { date: "2024-05-29", totalInspections: 78, detectedDefects: 130 },
+  { date: "2024-05-30", totalInspections: 340, detectedDefects: 280 },
+  { date: "2024-05-31", totalInspections: 178, detectedDefects: 230 },
+  { date: "2024-06-01", totalInspections: 178, detectedDefects: 200 },
+  { date: "2024-06-02", totalInspections: 470, detectedDefects: 410 },
+  { date: "2024-06-03", totalInspections: 103, detectedDefects: 160 },
+  { date: "2024-06-04", totalInspections: 439, detectedDefects: 380 },
+  { date: "2024-06-05", totalInspections: 88, detectedDefects: 140 },
+  { date: "2024-06-06", totalInspections: 294, detectedDefects: 250 },
+  { date: "2024-06-07", totalInspections: 323, detectedDefects: 370 },
+  { date: "2024-06-08", totalInspections: 385, detectedDefects: 320 },
+  { date: "2024-06-09", totalInspections: 438, detectedDefects: 480 },
+  { date: "2024-06-10", totalInspections: 155, detectedDefects: 200 },
+  { date: "2024-06-11", totalInspections: 92, detectedDefects: 150 },
+  { date: "2024-06-12", totalInspections: 492, detectedDefects: 420 },
+  { date: "2024-06-13", totalInspections: 81, detectedDefects: 130 },
+  { date: "2024-06-14", totalInspections: 426, detectedDefects: 380 },
+  { date: "2024-06-15", totalInspections: 307, detectedDefects: 350 },
+  { date: "2024-06-16", totalInspections: 371, detectedDefects: 310 },
+  { date: "2024-06-17", totalInspections: 475, detectedDefects: 520 },
+  { date: "2024-06-18", totalInspections: 107, detectedDefects: 170 },
+  { date: "2024-06-19", totalInspections: 341, detectedDefects: 290 },
+  { date: "2024-06-20", totalInspections: 408, detectedDefects: 450 },
+  { date: "2024-06-21", totalInspections: 169, detectedDefects: 210 },
+  { date: "2024-06-22", totalInspections: 317, detectedDefects: 270 },
+  { date: "2024-06-23", totalInspections: 480, detectedDefects: 530 },
+  { date: "2024-06-24", totalInspections: 132, detectedDefects: 180 },
+  { date: "2024-06-25", totalInspections: 141, detectedDefects: 190 },
+  { date: "2024-06-26", totalInspections: 434, detectedDefects: 380 },
+  { date: "2024-06-27", totalInspections: 448, detectedDefects: 490 },
+  { date: "2024-06-28", totalInspections: 149, detectedDefects: 200 },
+  { date: "2024-06-29", totalInspections: 103, detectedDefects: 160 },
+  { date: "2024-06-30", totalInspections: 446, detectedDefects: 400 },
 ];
 
 const chartConfig = {
   visitors: {
     label: "Visitors",
   },
-  desktop: {
+  totalInspections: {
     label: "총 검사",
     color: "hsl(var(--chart-2))",
   },
-  mobile: {
+  detectedDefects: {
     label: "검출 결함",
     color: "hsl(var(--chart-6))",
   },
@@ -185,27 +185,39 @@ function InspectionSummaryChart() {
         >
           <AreaChart data={filteredData}>
             <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+              <linearGradient
+                id="fillTotalInspections"
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="1"
+              >
                 <stop
                   offset="5%"
-                  stopColor="var(--color-desktop)"
+                  stopColor="hsl(var(--chart-2))"
                   stopOpacity={0.8}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-desktop)"
+                  stopColor="hsl(var(--chart-2))"
                   stopOpacity={0.1}
                 />
               </linearGradient>
-              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+              <linearGradient
+                id="fillDetectedDefects"
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="1"
+              >
                 <stop
                   offset="5%"
-                  stopColor="var(--color-mobile)"
+                  stopColor="hsl(var(--chart-6))"
                   stopOpacity={0.8}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-mobile)"
+                  stopColor="hsl(var(--chart-6))"
                   stopOpacity={0.1}
                 />
               </linearGradient>
@@ -240,16 +252,16 @@ function InspectionSummaryChart() {
               }
             />
             <Area
-              dataKey="desktop"
+              dataKey="totalInspections"
               type="natural"
-              fill="url(#fillDesktop)"
-              stroke="var(--color-desktop)"
+              fill="url(#fillTotalInspections)"
+              stroke="hsl(var(--chart-2))"
             />
             <Area
-              dataKey="mobile"
+              dataKey="detectedDefects"
               type="natural"
-              fill="url(#fillMobile)"
-              stroke="var(--color-mobile)"
+              fill="url(#fillDetectedDefects)"
+              stroke="hsl(var(--chart-6))"
             />
             <ChartLegend content={<ChartLegendContent />} />
           </AreaChart>
